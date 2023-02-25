d91dee81d5a38af14a2d6d8d695a3c31a234f050
Mezouari Adam <102043443+pennywise92@users.noreply.github.com>
2023-02-25 15:53:58 +0100

    Add files via upload
    data

---
+++ AchatEnLigneContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AchatEnLigne.Models;
+
+namespace AchatEnLigne.Data
+{
+    public class AchatEnLigneContext : DbContext
+    {
+        public AchatEnLigneContext (DbContextOptions<AchatEnLigneContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<AchatEnLigne.Models.LignePanierCommande> LignePanierCommande { get; set; }
+
+        public DbSet<AchatEnLigne.Models.Panier> Panier { get; set; }
+
+        public DbSet<AchatEnLigne.Models.Produit> Produit { get; set; }
+
+        public DbSet<AchatEnLigne.Models.User> User { get; set; }
+
+        public DbSet<AchatEnLigne.Models.Commande> Commande { get; set; }
+    }
+}
